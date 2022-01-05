73080d10dfca793b65fbbcf7f7199a03a97319b1
EJBubble <86179665+EJBubble@users.noreply.github.com>
2022-01-06 00:01:42 +0800

    Update all.py

--- all.py
+++ all.py
@@ -60,7 +60,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
  
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'EJ', 'PaulLiu', 'GoodGUY', 'Z', 'W'] 
+names = ['None', 'GoodGuy', 'GoodGuy', 'GoodGuy', 'GoodGuy', 'GoodGuy'] 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video widht
